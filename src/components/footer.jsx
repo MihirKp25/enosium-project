e5d0285bfe3e3bad5a33587999ca699972024f62
aditiganvir28 <aditi10328@gmail.com>
2023-01-17 23:21:16 +0530

    added hover effects

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ export const Footer=()=>{
     <div className="row-span-1  text-left ">
         <input placeholder="Enter your email Address" className="bg-[#e2e3e4] text-left rounded-md  w-[90%] h-[90%] ml-[3%] pl-[2%] text-black"></input>
     </div>
-    <div className="row-span-1 text-left">
+    {/* <div className="row-span-1 text-left">
         <motion.input type="submit" className="text-center border-solid rounded-md  w-[90%] h-[90%] ml-[3%] pl-[2%] bg-[#577de3] hover:bg-[#425fad] text-xl text-white font-semibold" value="Send" 
         whileHover={{
             scale: 1.03,
@@ -44,7 +44,20 @@ export const Footer=()=>{
         }}
         ></motion.input>
 
+    </div> */}
+    <div className="row-span-1 text-left footer-btn">
+    <button>
+  <div class="svg-wrapper-1">
+    <div class="svg-wrapper">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <path fill="none" d="M0 0h24v24H0z"></path>
+        <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+      </svg>
     </div>
+  </div>
+  <span>Send</span>
+</button>
+</div>
     <div className="row-span-1 "></div>
 
         </div>

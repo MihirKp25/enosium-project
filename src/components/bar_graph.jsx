cfa4bcf9d012cef0598c791aaf0ed20521a7b09b
aditiganvir28 <aditi10328@gmail.com>
2023-01-22 00:36:08 +0530

    intresr-rates added

---
+++ src/components/bar_graph.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import data from "../helpers/db.json"
+import { motion } from "framer-motion";
+
+export const Bar_graph=()=>{
+    const items= data;
+
+    return(
+        <div className="intrest-rates-bar-graph">
+          <h1>Intrest Rates on Car Loan</h1>
+        <div class="bar-graph bar-graph-vertical bar-graph-two">
+        <div class="bar-one bar-container">
+        <div className="bank-names">
+              {
+                items.map((item)=>{
+                  return(
+                    <div class="year"><p className="names
+                    ">{item.bankName}</p></div>
+                  )
+                })
+              }
+            
+            </div>
+          <div className="percentage-bar">
+            {
+                items.map((item)=>{
+                    return(
+                    
+                    <motion.div  initial={{width:0, height: "5%"}} animate={{width:item.intrest_rate*20, height:"5%"}} transition={{duration: 2, delay:0}} class="bar" data-percentage={`${item.intrest_rate}%`} style={{height:"5%"}}></motion.div>
+                    
+                    )
+                })
+            }
+            </div>
+           
+            </div>
+</div>
+        </div>
+    )
+}
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,4 +1,5 @@
 
+import { Bar_graph } from "./bar_graph"
 import { Footer } from "./footer"
 import { Forminput } from "./form"
 import { Navbar } from "./navbar"
@@ -8,6 +9,8 @@ export const Dashboard=()=>{
         <>
         <h1 className="text-center text-6xl font-bold p-5 text-[#d03ac1]">Welcome to dashboard</h1>
         <Forminput/>
+        
+        <Bar_graph/>
         <Footer/>
         </>
     )

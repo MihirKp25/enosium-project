2d4878f00f6021dae1929b7fa61d98259368dd25
aditiganvir28 <aditi10328@gmail.com>
2023-01-22 17:35:34 +0530

    back-button

---
+++ src/components/back-button.jsx
@@ -0,0 +1,12 @@
+export const Backbutton=()=>{
+    return(
+        <button>
+            <div class="arrow-wrapper">
+        <div class="arrow"></div>
+
+    </div>
+    Sign up
+    
+</button>
+    )
+}
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,4 +1,5 @@
 
+import { Backbutton } from "./back-button"
 import { Bar_graph } from "./bar_graph"
 import { Footer } from "./footer"
 import { Forminput } from "./form"
@@ -7,6 +8,7 @@ import { Navbar } from "./navbar"
 export const Dashboard=()=>{
     return(
         <>
+        <Backbutton/>
         <h1 className="text-center text-6xl font-bold p-5 text-[#d03ac1]">Welcome to dashboard</h1>
         <Forminput/>
         
